e774f0b4bea57fa845994ec05eaf18855d6eebfe
Iftikhar Ahmad <iftikharahmad.pir@gmail.com>
2021-09-21 00:30:15 +0200

    InterfaceLayer Added with React.js

---
+++ Xello.BusinessLayer/DataManipulation/InsertData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xello.DataLayer.Models;
+
+namespace Xello.BusinessLayer.DataManipulation
+{
+    public static class InsertData<T>
+    {
+        //public static bool InsertData<T>()
+        //{
+        //    using (var context = new XelloContext())
+        //    {
+        //        context.UserCredentials.Add(T);
+        //        context.SaveChanges();
+        //    }
+        //}
+    }
+}
